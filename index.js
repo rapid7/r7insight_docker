eb17eb08ffde398c77be576ef626d8966b91c9b8
Szymon Bialkowski <szymon_bialkowski@rapid7.com>
2022-04-25 16:44:42 +0100

    remove legacy test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const { Command } = require('commander');
 
 const UUID_REGEX = /^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$/;
 
-//  Winston logger initialised after CLI arg parsing
+//  Winston logger initialized after CLI arg parsing
 let LOGGER;
 
 
